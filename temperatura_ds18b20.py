39abd66a0c04bf82911071e8e3a8aebb7acf48b0
alexandre <alexandre.abreu@ifsc.edu.br>
2019-06-07 19:54:38 -0300

    log na nuvem e local para multiplos sensores com exceptions e autodetecção

--- temperatura_ds18b20.py
+++ temperatura_ds18b20.py
@@ -22,15 +22,25 @@ os.system('modprobe w1-therm')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 hostname = socket.gethostname()
 
+# Credenciais do Google Drive API
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(dir_path, 'secret_key.json'), scope)
+client = gspread.authorize(creds)
+# Abre uma o documeto (spreadsheet)
+spreadsheet = client.open(hostname)
+
+
 # Configuracoes dos diretorios W1
 base_dir = '/sys/bus/w1/devices/'
-device_folders = os.listdir(base_dir)
-device_folders.remove('w1_bus_master1')
 
 def read_temp_raw(device_file):
-	f = open(os.path.join(base_dir, device_file), 'r')
-	lines = f.readlines()[-1] # ultima linha somente
-	f.close()
+	try:
+		f = open(os.path.join(base_dir, device_file), 'r')
+		lines = f.readlines()[-1] # ultima linha somente
+		f.close()
+	except FileNotFoundError as fnf:
+		print(fnf)
+		print('Sensor removido =/')
 	return lines
  
 def read_temp(folder):
@@ -39,51 +49,49 @@ def read_temp(folder):
 	if equals_pos != -1:
 		temp_string = line[equals_pos + 2:]
 		temp_c = float(temp_string) / 1000
-		return (folder, temp_c)
-
-
-# Credenciais do Google Drive API
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(dir_path, 'secret_key.json'), scope)
-client = gspread.authorize(creds)
-# Abre uma o documeto (spreadsheet)
-spreadsheet = client.open(hostname)
-# Abre a planilha (worksheet)
-worksheet = spreadsheet.worksheet('temperatura-cpu')
+		return temp_c
 
 def getCPUtemperature():
 	res = os.popen('vcgencmd measure_temp').readline()
 	return(res.replace('temp=', '').replace("'C\n", ''))
 
 
-while (True):
-	temperatura = float(getCPUtemperature())
-
-	# data : hora, temperatura
-	row = [datetime.now().strftime('%d/%m/%Y %H:%M:%-S'), temperatura]
-
-	try :
-		worksheet.append_row(row)
-	except Exception as e:
-		print(e)
-		print('Erro ao enviar dados para a nuvem')
-
-	try:
-		with open(os.path.join(dir_path, 'log-temperatura.csv'), 'a') as f:
-			w = csv.writer(f)
-			w.writerow(row)
-	except Exception as e:
-		print(e)
-		print('Erro ao salvar dado em log-temperatura.csv')
-
-	time.sleep(10)
-
-
 # Loop principal
 while True:
+	device_folders = os.listdir(base_dir)
 	for folder in device_folders:
 		if folder[0:2] != '28':
-			#print(folder)
 			continue
-		print(read_temp(folder))
-		time.sleep(1)
+
+		# Todas as worksheets
+		ws_list = spreadsheet.worksheets()
+		worksheet = None
+
+		# Abre a planilha (worksheet)
+		for ws in ws_list:
+			if ws.title == folder:
+				worksheet = spreadsheet.worksheet(folder)
+				break
+		# Se nao existe, cria
+		if worksheet == None:
+			worksheet = spreadsheet.add_worksheet(title=folder, rows='100', cols='2')
+			worksheet.append_row(['Data Hora', 'Temperatura'])
+		
+		# data e hora, temperatura		
+		row = [datetime.now().strftime('%d/%m/%Y %H:%M:%-S'), read_temp(folder)]
+
+		try :
+			worksheet.append_row(row)
+		except Exception as e:
+			print(e)
+			print('Erro ao enviar dados para a nuvem')
+
+		try:
+			with open(os.path.join(dir_path, 'logs-temperatura' ,folder + '.csv'), 'a') as f:
+				w = csv.writer(f)
+				w.writerow(row)
+		except Exception as e:
+			print(e)
+			print('Erro ao salvar dado em arquivo .csv')
+
+		time.sleep(5)
