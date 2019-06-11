a410ef95dfcfad6679aa1b73e73479f42fe5ee88
alegume <alexandreabreu@comp.ufla.br>
2019-06-11 16:22:50 -0300

    tabas

--- gases-mq-2.py
+++ gases-mq-2.py
@@ -4,27 +4,27 @@ import time
 from gas_detection import GasDetection
 
 def main():
-    """Handle example."""
+	"""Handle example."""
 
-    print('Calibrating ...')
-    detection = GasDetection()
+	print('Calibrating ...')
+	detection = GasDetection()
 
-    try:
-        while True:
-            ppm = detection.percentage()
+	try:
+		while True:
+			ppm = detection.percentage()
 
-            print('CO: {} ppm'.format(ppm[detection.CO_GAS]))
-            print('H2: {} ppm'.format(ppm[detection.H2_GAS]))
-            print('CH4: {} ppm'.format(ppm[detection.CH4_GAS]))
-            print('LPG: {} ppm'.format(ppm[detection.LPG_GAS]))
-            print('PROPANE: {} ppm'.format(ppm[detection.PROPANE_GAS]))
-            print('ALCOHOL: {} ppm'.format(ppm[detection.ALCOHOL_GAS]))
-            print('SMOKE: {} ppm\n'.format(ppm[detection.SMOKE_GAS]))
+			print('CO: {} ppm'.format(ppm[detection.CO_GAS]))
+			print('H2: {} ppm'.format(ppm[detection.H2_GAS]))
+			print('CH4: {} ppm'.format(ppm[detection.CH4_GAS]))
+			print('LPG: {} ppm'.format(ppm[detection.LPG_GAS]))
+			print('PROPANE: {} ppm'.format(ppm[detection.PROPANE_GAS]))
+			print('ALCOHOL: {} ppm'.format(ppm[detection.ALCOHOL_GAS]))
+			print('SMOKE: {} ppm\n'.format(ppm[detection.SMOKE_GAS]))
 
-            time.sleep(0.25)
+			time.sleep(0.25)
 
-    except KeyboardInterrupt:
-        print('\nAborted by user!')
+	except KeyboardInterrupt:
+		print('\nAborted by user!')
 
 if __name__ == '__main__':
-main()
+	main()
